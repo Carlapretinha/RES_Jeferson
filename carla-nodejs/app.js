1b5bbc7336e4ea61283ca2f97f28a16e8c3ae295
Carlapretinha <carla.souza6@fatec.sp.gov.br>
2023-04-05 09:29:46 -0300

    1° commit

---
+++ carla-nodejs/app.js
@@ -0,0 +1,21 @@
+const express = require("express")
+const app = express()
+const handlebars = require("express-handlebars").engine
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}))
+app.set("view engine", "handlebars")
+
+app.get("/", function(req,res){
+    res.render("primeira_pagina")
+})
+
+app.get("/segunda", function(req,res){
+    res.render("segunda_pagina")
+})
+app.post("/cadastrar", function(req,res){
+    res.send("formulario recebido")
+})
+
+app.listen(8081, function(){
+    console.log("Servidor ativo")
+})
